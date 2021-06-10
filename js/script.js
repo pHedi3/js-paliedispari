6702ff39247f7ba33068eda7313c743ca51adca4
pHedi3 <federico.gualdi3@gmail.com>
2021-06-10 10:57:42 +0200

    semplificato

--- js/script.js
+++ js/script.js
@@ -27,25 +27,26 @@ if (isPalindrom(userWord)) {
 }
 
 // parte del pari
+
+
+var userChoise = lowCaseWord( prompt('segli pari o dispari'))
+while (userChoise != 'pari' && userChoise != 'dispari') {
+    alert('devi scegliere pari o dispari')
+    userChoise = lowCaseWord( prompt('segli pari o dispari'))
+}
+
 var userNumber = parseInt(prompt('inserisci un numero da 1 a 5'));
-while(isNaN(userNumber) || userNumber < 0 || userNumber > 5) {
+while(isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
     alert('devi inserire un numero corretto');
     userNumber = parseInt(prompt('inserisci un numero da 1 a 5'));
 }
 
 var tot = userNumber + randomNumber5();
-var flag = false;
-while (userChoise != 'pari' && userChoise != 'dispari') {
-    var userChoise = lowCaseWord( prompt('segli pari o dispari'))
-    if (('pari' == userChoise && tot % 2 == 0) || ('dispari' == userChoise && tot % 2 != 0 ) ) {
-        flag = true;
-    } else if (userChoise != 'pari' && userChoise != 'dispari') {
-        alert('devi fare una scelta');
-    }
-}
-if (flag) {
+
+if (('pari' == userChoise && tot % 2 == 0) || ('dispari' == userChoise && tot % 2 != 0 ) ) {
     resultElement2.innerHTML = 'hai vinto'
 } else {
     resultElement2.innerHTML = 'hai perso'
 }
+
 
